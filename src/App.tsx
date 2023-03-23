d779863d85adda0af09fe7fa3b72e7fc329b7045
Brianidle <ddbrayan10@hotmail.com>
2023-03-23 18:36:25 -0400

    Refactor ball animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,87 +1,30 @@
-import React, { useState } from "react";
-import styled, { keyframes } from "styled-components";
-import { animated, useTransition } from "react-spring";
+import React, { CSSProperties, useEffect, useState } from "react";
+import styled from "styled-components";
 
-import "./App.css";
 import fireball from "./assets/fire-fireball.gif";
-import Fireballs from "./components/Fireballs";
 
-const burnDeedResultFakeData = [
-  {
-    deedTokenId: "IDEED_3122",
-    result: {
-      type: "Infrastructure",
-      prefix: "MIMF",
-      no: 298,
-      name: "Mining Facility",
-      location: "Moon",
-      moonCycle: 1,
-      origin: "Moon",
-      artist: "GIN",
-      description:
-        "The start of any successful Moon Team's building plan. TTM tokens are mined and make the moon go round. This infrastructure is a well-oiled machine running on Lithium Moon Rocks, as it maintains the economic system of TOTHEMOON UNIVERSE.",
-      image:
-        "https://ipfs.tothemoon.net/ipfs/QmVoqyLghYsw5PVSr7kyfYeyA9Bt575pdJLdjeHeuapETX",
-    },
-  },
-  {
-    deedTokenId: "IDEED_3123",
-    result: {
-      type: "Infrastructure",
-      prefix: "MICT",
-      no: 140,
-      name: "Communication Tower",
-      location: "Moon",
-      moonCycle: 1,
-      origin: "Moon",
-      artist: "GIN",
-      description:
-        "No dropped calls here. Signal is strengthened as these towers offer a greater radius of communication on the moon. With cell service this good, teams are able to quickly and easily connect with each other.",
-      image:
-        "https://ipfs.tothemoon.net/ipfs/QmQo2MXRD5dcjCbH4vy4JkDRi6udELXE4skVzjC5uYkjCT",
-    },
-  },
-  {
-    deedTokenId: "IDEED_3124",
-    result: null,
-  },
-  {
-    deedTokenId: "IDEED_3125",
-    result: null,
-  },
-  {
-    deedTokenId: "IDEED_3124",
-    result: null,
-  },
-  {
-    deedTokenId: "IDEED_3125",
-    result: null,
-  },
-  {
-    deedTokenId: "IDEED_3124",
-    result: null,
-  },
-  {
-    deedTokenId: "IDEED_3125",
-    result: null,
-  },
-  {
-    deedTokenId: "IDEED_3124",
-    result: null,
-  },
-  {
-    deedTokenId: "IDEED_3125",
-    result: null,
-  },
-];
+const BALL_SIZE = 18;
+const NUMBER_OF_BALLS = 9;
 
-const fade = keyframes`
-0% {
-    opacity: 0;
-  }
-  100% {
-    opacity: 1;
-  }
+const RedLight = styled.li`
+  background-color: #f60c24;
+  position: absolute;
+  top: -150px;
+  left: 185px;
+  width: ${BALL_SIZE}px;
+  height: ${BALL_SIZE}px;
+  border-radius: 50%;
+  list-style: none;
+  transform-origin: 50% 50%;
+  filter: blur(2px);
+  transition: all 0.5s ease-in-out;
+`;
+
+const BallContainers = styled.ul`
+  position: relative;
+  max-width: 350px;
+  width: 100%;
+  margin-top: 60px;
 `;
 
 const Container = styled.div`
@@ -96,8 +39,6 @@ const DeedCardImageSuccess = styled.img`
   max-width: 400px;
   width: 100%;
   height: 100%;
-  -webkit-animation: ${fade} 1.2s cubic-bezier(0.39, 0.575, 0.565, 1) both;
-  animation: ${fade} 1.2s cubic-bezier(0.39, 0.575, 0.565, 1) both;
   z-index: 99;
   cursor: pointer;
 `;
@@ -112,12 +53,72 @@ const Center = styled.div`
   }
 `;
 
-function App() {
-  const [active, setActive] = useState(false);
+export const sleep = (ms: number) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
 
-  setTimeout(() => {
-    setActive(true);
-  }, 3000);
+function App() {
+  const [appliedStyles, setAppliedStyles] = useState<
+    { key: number; value: CSSProperties }[]
+  >([]);
+
+  const [styles, setStyles] = useState<{ key: number; value: CSSProperties }[]>(
+    []
+  );
+
+  const setLightsAnimation = (numberOfBalls: number) => {
+    let styles: { key: number; value: CSSProperties }[] = [];
+    let appliedStyles: { key: number; value: CSSProperties }[] = [];
+    let startPosition = 0;
+    let separationBetweenBalls = 30;
+
+    startPosition = Math.floor(
+      (numberOfBalls * BALL_SIZE +
+        (numberOfBalls - 1) * separationBetweenBalls) /
+        3.34
+    );
+
+    for (let i = 0; i < numberOfBalls; i++) {
+      let style: CSSProperties = {
+        transform: `translate(${
+          -startPosition + separationBetweenBalls * i
+        }px, 140px)`,
+      };
+
+      styles.push({ key: i, value: style });
+      appliedStyles.push({ key: i, value: {} });
+    }
+    setAppliedStyles(appliedStyles);
+    setStyles(styles);
+  };
+
+  const playBallAnimation = async (numberOfBalls: number) => {
+    for (let i = 0; i < numberOfBalls; i++) {
+      setAppliedStyles((prevState) => {
+        const newState = prevState.map((appliedStyle) => {
+          if (appliedStyle.key == i) {
+            return { ...appliedStyle, value: styles[i].value };
+          }
+
+          return appliedStyle;
+        });
+
+        return newState;
+      });
+      await sleep(600);
+    }
+  };
+
+  const resetBallAnimation = () => {
+    setStyles([]);
+    setAppliedStyles([]);
+  };
+
+  useEffect(() => {
+    if (styles.length > 0) {
+      playBallAnimation(NUMBER_OF_BALLS);
+    }
+  }, [styles]);
 
   return (
     <Container>
@@ -129,19 +130,30 @@ function App() {
           width: "100%",
         }}
       >
-        <DeedCardImageSuccess className="fade-in" src={fireball} />
-        <ul className="fireballs">
-          {burnDeedResultFakeData.map((data: any, index: number) => {
-            return (
-              <Fireballs
-                key={index}
-                index={index}
-                isVisible={active ? true : false}
-                data={data}
-              ></Fireballs>
-            );
-          })}
-        </ul>
+        <DeedCardImageSuccess src={fireball} />
+        <BallContainers>
+          <RedLight />
+          {appliedStyles.length > 0 &&
+            appliedStyles.map((style) => (
+              <RedLight key={style.key} style={style.value} />
+            ))}
+        </BallContainers>
+        <button
+          style={{ marginTop: "20px" }}
+          onClick={() => {
+            setLightsAnimation(NUMBER_OF_BALLS);
+          }}
+        >
+          play animation
+        </button>
+        <button
+          style={{ marginTop: "20px" }}
+          onClick={() => {
+            resetBallAnimation();
+          }}
+        >
+          reset animation
+        </button>
       </Center>
     </Container>
   );
